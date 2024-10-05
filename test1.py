2d9e92ca960fe7ea5dc6ea7e5a8e8cc868070a5f
liuzhihaotina <liuzhihaotina@163.com>
2024-10-05 21:48:26 +0800

    Modified test1

--- test1.py
+++ test1.py
@@ -11,29 +11,11 @@ distance_list=[0]*num
 # # 先打开我们的目标表格，再打开我们的目标表单
 wb=openpyxl.load_workbook(rf'第{numb}章作业.xlsx')
 ws = wb[f'第{numb}章作业（收集结果）']
+c=ws.max_column+1  # 新增一列，存储发送次数
 # # 取出distance_list列表中的每一个元素，openpyxl的行列号是从1开始取得，所以我这里i从1开始取
 for i in range(1,len(distance_list)+1):
     distance=distance_list[i-1]
     # 写入位置的行列号可以任意改变，这里我是从第2行开始按行依次插入第11列
-    ws.cell(row = i+1, column = 7).value =0
+    ws.cell(row = i+1, column = c).value =0
 # # 保存操作
 wb.save(rf'第{numb}章作业.xlsx')
-#
-# # o_list=[]
-# # nam='张三'
-# # num=1
-# # o_list.append(f'{num}' '-' f'{nam}')
-# # print(o_list)
-#
-#
-# # df = pandas.read_excel(f'第五章作业提交网址.xlsx')
-# # for i in range(len(df)):
-# #     # print(df['发送次数'].values[i])
-# #     df['发送次数'].values[i]=0
-# # df.save(r'第五章作业提交网址.xlsx')
-# # # print(df['发送次数'].values)
-
-# lisr=[]
-# lisr.append('d')
-# if lisr:
-#     print('执行了这个语句')
